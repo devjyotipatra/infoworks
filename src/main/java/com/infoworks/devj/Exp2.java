e0f430ff1aa0ecb3866f956fbd5233f684f4c553
devjyotip <devjyotip@qubole.com>
2018-05-24 21:27:46 +0530

    partial exp2

--- src/main/java/com/infoworks/devj/Exp2.java
+++ src/main/java/com/infoworks/devj/Exp2.java
@@ -19,19 +19,21 @@ public class Exp2 {
   Map<String, String> MAP_SUFFIX= new HashMap();
 
 
-  private void generateSuffixes(String str) {
+  private void generateSuffixeToPrefixMapping(String str) {
     if(str.length() <= 2) {
       MAP_SUFFIX.put(str, "");
       return;
     }
 
     for(int i=str.length()-2; i>=0; i--) {
-      String subStr = str.substring(str.length()-1, i)
-      MAP_SUFFIX.put();
+      String subStrSuffix = str.substring(str.length()-1, i);
+      String subStrPrefix = str.substring(0, i+1);
+
+      MAP_SUFFIX.put(subStrSuffix, subStrPrefix);  // (suffix -> prefix)
     }
   }
 
-  private void generateAndMatchPrefixes(int strId, String str) {
+  private void generateAndMatchPrefixes(String str) {
     List<String> suffixFound = new ArrayList();
 
     for(int i=1; i<str.length(); i++) {
@@ -44,6 +46,15 @@ public class Exp2 {
 
   public String getMinLengthString(String[] arr) {
     String res = "";
+
+    for(int i=0; i<arr.length; i++) {
+      generateSuffixeToPrefixMapping(arr[i]);  // (suffix -> prefix)
+    }
+
+
+    for(int i=0; i<arr.length; i++) {
+      generateAndMatchPrefixes(arr[i]);  
+    }
 
     return res;
   }
